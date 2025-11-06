16284c37c1c7d5072ce1adb51d024e8488df137c
Rizve Rahman Reza <rizverahman.ai@outlook.com>
2025-11-06 13:05:56 +0600

    Add project files.

---
+++ MvcDemo/Controllers/ProfileController.cs
@@ -0,0 +1,29 @@
+﻿using MvcDemo.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Index()
+        {
+
+            var users = new List<User> {
+
+                new User { Id = 1, Name = "Alice", Email = "dfsldfjs" },
+                new User { Id = 2, Name = "Bob", Email = "dfkjooei" },
+                new User { Id = 3, Name = "Charlie", Email = "dfjlsdfj" }
+            };
+
+
+            return View(users);
+        }
+
+        public ActionResult Social()
+        {
+            return View();
+        }
+    }
+}
