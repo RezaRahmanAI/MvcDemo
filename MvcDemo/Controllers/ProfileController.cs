16c04a2dc1c4474934a68928aa70a74fe8e35943
Rizve Rahman Reza <rizverahman.ai@outlook.com>
2025-11-09 10:50:40 +0600

    running

--- MvcDemo/Controllers/ProfileController.cs
+++ MvcDemo/Controllers/ProfileController.cs
@@ -6,24 +6,36 @@ namespace MvcDemo.Controllers
 {
     public class ProfileController : Controller
     {
-        // GET: Profile
-        public ActionResult Index()
-        {
 
-            var users = new List<User> {
+        static List<User> users = new List<User> {
 
                 new User { Id = 1, Name = "Alice", Email = "dfsldfjs" },
                 new User { Id = 2, Name = "Bob", Email = "dfkjooei" },
                 new User { Id = 3, Name = "Charlie", Email = "dfjlsdfj" }
-            };
+         };
 
-
+        // GET: Profile
+        public ActionResult Index()
+        {
             return View(users);
         }
 
-        public ActionResult Social()
+        public ActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            user.Id = users.Count + 1;
+            users.Add(user);
+            return RedirectToAction("Index");
+        }
+
+        //public ActionResult Social()
+        //{
+        //    return View();
+        //}
     }
 }
